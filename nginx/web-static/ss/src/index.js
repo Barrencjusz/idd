367a633381b139dc45464facaaa4aecf4518cd9c
Piotr Larysz <piotr.larysz@performgroup.com>
2017-03-05 22:34:22 +0100

    logspout + react web update

--- nginx/web-static/ss/src/index.js
+++ nginx/web-static/ss/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ActionsSearch from './ActionsSearch';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <ActionsSearch />,
   document.getElementById('root')
 );
--- rest-repository/src/main/java/pl/piteron/repository/ActionRepository.java
+++ rest-repository/src/main/java/pl/piteron/repository/ActionRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.piteron.entity.Action;
 
 /**
  * @author piotr.larysz
  */
+@CrossOrigin
 @RepositoryRestResource
 public interface ActionRepository extends PagingAndSortingRepository<Action, String> {
 
